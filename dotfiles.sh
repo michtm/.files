2b48b603c7c72b8ce920069b32e00fc3e1145737
Michel Tring <michel.tring@gmail.com>
2024-09-11 06:49:11 +0200

    (dotfiles)(fr) Copie récursive

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,16 +3,16 @@
 
 # Liste des fichiers points
 dotfiles=(
-	"bash:$HOME"
-	"git:$HOME"
-	"nvim:$HOME/.config/nvim"
+  "bash:$HOME"
+  "git:$HOME"
+  "nvim:$HOME/.config/nvim"
 )
 an_error_occurs=0
 
 # Fonctions
 dotfiles_usage()
 {
-	cat <<EOF
+  cat <<EOF
 Usage: ${0##*/} <command> [<dotfiles> ...]
 
 Liste des commandes disponibles:
@@ -27,108 +27,108 @@ EOF
 
 dotfiles_doit()
 {
-	option=$1
-	shift
-	case $option in
-		-D)
-			action="Unstowing"
-			;;
-		-R)
-			action="Stowing"
-			;;
-	esac
-	if [ $# -eq 0 ];
-	then
-		echo "[$FUNCNAME] $action all..."
-		for a_dotfiles in "${dotfiles[@]}";
-		do
-			source_directory="${a_dotfiles%%:*}"
-			target_directory="${a_dotfiles##*:}"
-			backup="backup/$source_directory/$(date +%F)"
-			mkdir -p $backup
-			for file in $(ls -A $source_directory);
-			do
-				if [ -L "$target_directory/$file" ];
-				then
-					target=$(realpath "$target_directory/$file")
-					cp $target $backup
-				else
-					mv "$target_directory/$file" $backup 2>/dev/null
-				fi
-			done
-			echo "[$FUNCNAME]   - $action $source_directory..."
-			if [ "$target_directory" != "$HOME" ];
-			then
-				mkdir -p $target_directory
-			fi
-			stow -v $option -t $target_directory $source_directory
-		done
-	else
-		for a_dotfiles in $@
-		do
-			for template_dotfiles in "${dotfiles[@]}";
-			do
-				source_directory="${template_dotfiles%%:*}"
-				target_directory="${template_dotfiles##*:}"
-				if [ "$source_directory" == "$a_dotfiles" ];
-				then
-					break
-				else
-					target_directory=
-				fi
-			done
-			if [ -z "$target_directory" ];
-			then
-				echo "[$FUNCNAME] Error: could not found target directory for '$a_dotfiles'"
-			else
-				backup="backup/$source_directory/$(date +%F)"
-				mkdir -p $backup
-				for file in $(ls -A $source_directory);
-				do
-					if [ -L "$target_directory/$file" ];
-					then
-						target=$(realpath "$target_directory/$file")
-						cp $target $backup
-					else
-						mv "$target_directory/$file" $backup 2>/dev/null
-					fi
-				done
-				echo "[$FUNCNAME] $action $source_directory..."
-				if [ "$target_directory" != "$HOME" ];
-				then
-					mkdir -p $target_directory
-				fi
-				stow -v $option -t $target_directory $source_directory
-			fi
-		done
-	fi
+  option=$1
+  shift
+  case $option in
+    -D)
+      action="Unstowing"
+      ;;
+    -R)
+      action="Stowing"
+      ;;
+  esac
+  if [ $# -eq 0 ];
+  then
+    echo "[$FUNCNAME] $action all..."
+    for a_dotfiles in "${dotfiles[@]}";
+    do
+      source_directory="${a_dotfiles%%:*}"
+      target_directory="${a_dotfiles##*:}"
+      backup="backup/$source_directory/$(date +%F)"
+      mkdir -p $backup
+      for file in $(ls -A $source_directory);
+      do
+        if [ -L "$target_directory/$file" ];
+        then
+          target=$(realpath "$target_directory/$file")
+          cp -r $target $backup
+        else
+          mv "$target_directory/$file" $backup 2>/dev/null
+        fi
+      done
+      echo "[$FUNCNAME]   - $action $source_directory..."
+      if [ "$target_directory" != "$HOME" ];
+      then
+        mkdir -p $target_directory
+      fi
+      stow -v $option -t $target_directory $source_directory
+    done
+  else
+    for a_dotfiles in $@
+    do
+      for template_dotfiles in "${dotfiles[@]}";
+      do
+        source_directory="${template_dotfiles%%:*}"
+        target_directory="${template_dotfiles##*:}"
+        if [ "$source_directory" == "$a_dotfiles" ];
+        then
+          break
+        else
+          target_directory=
+        fi
+      done
+      if [ -z "$target_directory" ];
+      then
+        echo "[$FUNCNAME] Error: could not found target directory for '$a_dotfiles'"
+      else
+        backup="backup/$source_directory/$(date +%F)"
+        mkdir -p $backup
+        for file in $(ls -A $source_directory);
+        do
+          if [ -L "$target_directory/$file" ];
+          then
+            target=$(realpath "$target_directory/$file")
+            cp -r $target $backup
+          else
+            mv "$target_directory/$file" $backup 2>/dev/null
+          fi
+        done
+        echo "[$FUNCNAME] $action $source_directory..."
+        if [ "$target_directory" != "$HOME" ];
+        then
+          mkdir -p $target_directory
+        fi
+        stow -v $option -t $target_directory $source_directory
+      fi
+    done
+  fi
 }
 
 # Programme principal
 while true;
 do
-	case "$1" in
-		help|"")
-			dotfiles_usage
-			break
-			;;
+  case "$1" in
+    help|"")
+      dotfiles_usage
+      break
+      ;;
 
-		install)
-			shift
-			dotfiles_doit -R $@
-			break
-			;;
-		remove)
-			shift
-			dotfiles_doit -D $@
-			break
-			;;
-		*)
-			echo "${0##*/}: Invalid command: $1"
-			an_error_occurs=1
-			break
-			;;
-	esac
+    install)
+      shift
+      dotfiles_doit -R $@
+      break
+      ;;
+    remove)
+      shift
+      dotfiles_doit -D $@
+      break
+      ;;
+    *)
+      echo "${0##*/}: Invalid command: $1"
+      an_error_occurs=1
+      break
+      ;;
+  esac
 done
 
 unset -v dotfiles
